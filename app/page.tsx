f06f274c792fd26e3f39bf0e7874ec88f89d3a30
AbiodunOlaoluwa <mate.olaoluwa@gmail.com>
2024-08-28 14:15:25 +0100

    Started working on the login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
 import Image from "next/image";
+import devlinksTextLogo from "@/public/devlinks.svg";
+import devlinksVectorLogo from "@/public/devlinksLogoImage.svg";
+import "./page.css";
 
 export default function Home() {
   return (
-    <div>landing/login page</div>
+    <div className="loginPageContainer">
+      <div className="loginFormCardContainer">
+        <div className="loginFormLogoContainer flex gap-3 items-center justify-center">
+          <Image
+            width={40}
+            height={40}
+            src={devlinksVectorLogo}
+            alt="devlinksLogo"
+            className="loginLogoImage"
+          />
+          <Image
+          height={26}
+            src={devlinksTextLogo}
+            alt="devlinks"
+            className="loginLogoText"
+          />
+        </div>
+        <div className="loginFormCard mt-20 bg-white p-8 rounded-xl">login form</div>
+      </div>
+    </div>
   );
 }
