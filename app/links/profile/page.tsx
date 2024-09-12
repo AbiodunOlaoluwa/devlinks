9f45823cb3b6215d973fbab111513ab1010d8ba7
AbiodunOlaoluwa <mate.olaoluwa@gmail.com>
2024-09-12 11:36:20 +0100

    Did the profile page desktop ui

--- app/links/profile/page.tsx
+++ app/links/profile/page.tsx
@@ -2,6 +2,8 @@
 import "./profile.css";
 import DeviceLinksPreview from "@/app/components/DeviceLinksPreview";
 import { useLinkContext } from "../LinkContext";
+import Image from "next/image";
+import imageUploadBox from "@/app/images/uploadImageBox.svg";
 
 const Profile = () => {
 
@@ -16,10 +18,42 @@ const Profile = () => {
           <p className="headingP">Add your details to create a personal touch to your profile.</p>
         </div>
         <div className="editPanelBody">
-
+          <div className="profileSubArea">
+            <p className="profilePhotoText">Profile photo</p>
+            <div className="uploadImageBoxContainer">
+              <Image
+                className="imageUploadBox"
+                src={imageUploadBox}
+                alt="imageUploadBox"
+              />
+              <p className="uploadImageBoxText">
+                Image must be below 1024x1024px. <br />Use PNG or JPG format.
+              </p>
+            </div>
+          </div>
+          <div className="profileTextSubArea">
+            <div className="profileTextContainer">
+              <p className="profileText">First Name*</p>
+              <div className="textInputContainer">
+                <input required type="text" className="textInput" name="firstName" placeholder="e.g. John" />
+              </div>
+            </div>
+            <div className="profileTextContainer">
+              <p className="profileText">Last Name*</p>
+              <div className="textInputContainer">
+                <input required type="text" className="textInput" name="lastName" placeholder="e.g. Appleseed" />
+              </div>
+            </div>
+            <div className="profileTextContainer">
+              <p className="profileText">Email</p>
+              <div className="textInputContainer">
+                <input type="email" className="textInput" name="email" placeholder="e.g. email@example.com" />
+              </div>
+            </div>
+          </div>
         </div>
-        <hr />
         <div className="profileSavePanel">
+          <hr />
           <button className="profileSaveButton">Save</button>
         </div>
       </div>
